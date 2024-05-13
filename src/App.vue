22c09ea71eb23538eef8cd311a1bd848a2d5db8f
liorabel22 <60816560+liorabel22@users.noreply.github.com>
2024-05-13 20:31:04 +0300

    done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
+<script setup>
+  import { inject } from "vue";
+
+  const GStore = inject("GStore"); // <----
+</script>
+
 <template>
   <div id="app">
+    <div id="flashMessage" v-if="GStore.flashMessage">
+      {{ GStore.flashMessage }}
+    </div>
     <div id="nav">
       <router-link :to="{ name: 'EventList' }">Events</router-link> |
       <router-link :to="{ name: 'About' }">About</router-link>
@@ -9,28 +18,41 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @keyframes yellowfade {
+    from {
+      background: yellow;
+    }
+    to {
+      background: transparent;
+    }
+  }
 
-#nav {
-  padding: 30px;
-}
+  #flashMessage {
+    animation-name: yellowfade;
+    animation-duration: 3s;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #nav {
+    padding: 30px;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
 
-h4 {
-  font-size: 20px;
-}
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+
+  h4 {
+    font-size: 20px;
+  }
 </style>
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,17 +1,22 @@
 <script setup>
-import { useRouter } from "vue-router";
-import { defineProps } from "vue";
+  import { useRouter } from "vue-router";
+  import { defineProps, inject } from "vue";
 
-defineProps(["event"]);
-const router = useRouter();
+  const props = defineProps(["event"]);
+  const router = useRouter();
+  const GStore = inject("GStore");
 
-const register = () => {
-  // Call to API
-  // If registered then redirect to event details
-  router.push({
-    name: "EventDetails",
-  });
-};
+  const register = () => {
+    // Call to API
+    // If registered then redirect to event details
+    GStore.flashMessage = `You are successfully registered for ${props.event.title}`;
+    setTimeout(() => {
+      GStore.flashMessage = "";
+    }, 3000);
+    router.push({
+      name: "EventDetails",
+    });
+  };
 </script>
 <template>
   <p>Register for the event here</p>
